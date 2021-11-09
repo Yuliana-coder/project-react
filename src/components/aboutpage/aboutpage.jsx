e18436f0d5749f17f8029efd6085d82d014af01a
unknown <Yuliana-coder>
2021-11-09 21:19:28 +0300

    2дз запись в прачечную, 3дз о нас

--- src/components/aboutpage/aboutpage.jsx
+++ src/components/aboutpage/aboutpage.jsx
@@ -1,19 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import { Typography } from "antd";
 
-export class Aboutpage extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const useGithubData = (userName) => {
+    const [userData, setUserData] = useState([]);
+    const token = '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';
 
-    render() {
-        return (
-            <div >
-                <Typography.Title>
-                    О нас
-                </Typography.Title>
-               Студент группы М05-116б. Ссылка на гитхаб <a href="https://github.com/Yuliana-coder" target="_blank">Github</a>
-            </div>
-        );
-    }
+    useEffect(() => {
+        if (!userName) return;
+        const fetchData = async () => {
+            const response = await axios(
+                'https://api.github.com/users/' + userName,
+                    {
+                        headers: {
+                        "Authorization": "token "+ token
+                    }
+                }
+            );
+            console.log(response.data);
+            setUserData(response.data);
+        };
+
+        fetchData();
+    }, [userName]);
+
+    return userData;
+}
+
+export function Aboutpage() {
+    const profileData = useGithubData('yuliana-coder');
+
+    return <div >
+    <Typography.Title>
+        О нас
+    </Typography.Title>
+        <Typography.Text>
+            Студент группы М05-116б. Ссылка на гитхаб 
+        </Typography.Text>
+        <Typography.Link href="https://github.com/Yuliana-coder" target="_blank">
+            Github
+        </Typography.Link>
+        <div>
+            <Typography.Text strong>Логин github</Typography.Text> {profileData.login}
+        </div>
+        <div>
+            <Typography.Text strong>BIO github</Typography.Text> {profileData.bio}
+        </div>
+    </div>;
 }
--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,22 +1,64 @@
-import React from "react";
-import { Typography } from "antd";
+import React, { useState } from "react";
+import { Typography, Input, Form, Button, Space, DatePicker } from "antd";
+import moment from 'moment';
 
-export class Homepage extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+function disabledDate(current) {
+    return current && current.valueOf() < Date.now() - Math.pow(60,4);
+}
 
-    render() {
-        return (
+export function Homepage() { 
+    let [userName, setUserName] = useState('')
+    let [date, setDate] = useState(0)
+    let [showMessage, setShowMessage] = useState('');
+
+    return <div>
+            <Typography.Title>
+                Записаться на стирку
+            </Typography.Title>
+            <Typography.Paragraph>
+                Здесь можно записаться в прачечную. Заполните Ваше имя и выберите удобное время стирки.
+            </Typography.Paragraph>
             <div>
-                <Typography.Title>
-                    Главная
-                </Typography.Title>
+                <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ remember: true }}
+                    autoComplete="off"
+                >
+                <Form.Item
+                    label="Имя"
+                    name="userName"
+                    rules={[{ required: true, message: 'Пожалуйста, введите Ваше имя' }]}
+                >
+                    <Input onInput={(event) => { setUserName(event.target.value) }} />
+                </Form.Item>
+
+                <Form.Item
+                    label="Время стирки"
+                    name="date"
+                    rules={[{ required: true, message: 'Пожалуйста, выберите дату и время стирки' }]}
+                >
+                    <DatePicker
+                        format={'YYYY-MM-DD HH:mm'}
+                        disabledDate={disabledDate}
+                        minuteStep={30}
+                        onChange={(event) => setDate(event._d)} showTime />
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button onClick={() => { setShowMessage(userName + ', ' + 'Вы успешно записаны на стирку. Дата: ' + date) }} type="primary" htmlType="submit">
+                    Записаться!
+                    </Button>
+                </Form.Item>
+                </Form>
+                {userName && date ? 
                 <Typography.Paragraph>
-                    Проект на React. 1 домашнее задание.
+                    {showMessage}
                 </Typography.Paragraph>
+                : null
+                }
             </div>
-        );
-    }
+        </div>
 }
 
